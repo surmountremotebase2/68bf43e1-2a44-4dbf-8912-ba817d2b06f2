7b889503ca9d7c4a6a2c880bd39e2eb2d738ae46
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-05 11:59:36 -0700

    User Simone Pantaleoni saved changes to main.py at 08/05/2024, 18:59:35 UTC time.

--- 30d60167-3d7b-4269-8b05-87feeba44e70/main.py
+++ 30d60167-3d7b-4269-8b05-87feeba44e70/main.py
@@ -1,6 +1,7 @@
 from surmount.base_class import Strategy, TargetAllocation, backtest
 from surmount.logging import log
 from datetime import datetime
+import pandas as pd
 
 class TradingStrategy(Strategy):
 
@@ -28,18 +29,35 @@ class TradingStrategy(Strategy):
     def assets(self):
         return self.tickers + self.bench
 
+    def realized_volatility_daily(series_log_return):
+        """
+        Get the daily realized volatility which is calculated as the square root
+        of sum of squares of log returns within a specific window interval 
+        """
+        n = len(series_log_return)
+        return np.sqrt(np.sum(series_log_return**2)/(n - 1))        
+
+
     def run(self, data):
         self.count += 7
-        
-        spy_data = data["SPY"]
-        spy_data['TR'] = np.maximum(spy_data['High'] - spy_data['Low'], np.maximum(spy_data['High'] - spy_data['Close'].shift(1), spy_data['Close'].shift(1) - spy_data['Low']))
-        spy_data['ATR'] = spy_data['TR'].rolling(window=14).mean()
-        spy_data['Returns'] = spy_data['Close'].pct_change()
-        spy_data['Realized Volatility'] = spy_data['Returns'].rolling(window=252).std() * np.sqrt(252)
+        spy_data = [entry['SPY'] for entry in data['ohlcv'] if 'SPY' in entry]
+        spy_data = pd.DataFrame(spy_data.Close, index=spy_data.date)
+        df['returns'] = 100 * spy_data.Close.pct_change().dropna()
+        # CALCULATE LOG RETURNS BASED ON ABOVE FORMULA
+        df['log_returns'] = np.log(spy_data.Close/spy_data.Close.shift(1))
+                
+        INTERVAL_WINDOW = 30
+        n_future = 7
 
-        # Calculate the 7th and 8th deciles
-        atr_deciles = np.percentile(spy_data['ATR'].dropna(), [70, 80])
-        vol_deciles = np.percentile(spy_data['Realized Volatility'].dropna(), [70, 80])
+        # GET BACKWARD LOOKING REALIZED VOLATILITY
+        df['vol_current'] = df.log_returns.rolling(window=INTERVAL_WINDOW)\
+                                        .apply(realized_volatility_daily)
+
+        # GET FORWARD LOOKING REALIZED VOLATILITY 
+        df['vol_future'] = df.log_returns.shift(-n_future)\
+                                        .rolling(window=INTERVAL_WINDOW)\
+                                        .apply(realized_volatility_daily)
+                                        
 
         # Check if the current ATR or Realized Volatility is above the 7th or 8th decile
         if spy_data['ATR'].iloc[-1] > atr_deciles[1] or spy_data['Realized Volatility'].iloc[-1] > vol_deciles[1]:
