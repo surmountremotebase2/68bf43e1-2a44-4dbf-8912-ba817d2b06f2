dffd071f29b62425bdf2e2f66c6f1c8ee8fee052
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-12 11:15:41 -0700

    User Simone Pantaleoni saved changes to main.py at 08/12/2024, 18:15:41 UTC time.

--- 0ac07374-f793-4b8f-85da-7da100084489/main.py
+++ 0ac07374-f793-4b8f-85da-7da100084489/main.py
@@ -34,7 +34,7 @@ class TradingStrategy(Strategy):
       today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
       yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
 
-      allocation_dict = {ticker: 0 for ticker in self.tickers}
+      allocation_dict = {}
       spy_data = [entry['SPY']['close'] for entry in data['ohlcv'] if 'SPY' in entry]
       spy_data = pd.DataFrame(spy_data, columns=['close'])
       spy_data['log_returns'] = np.log(spy_data.close/spy_data.close.shift(1))
@@ -64,16 +64,15 @@ class TradingStrategy(Strategy):
                self.count = 20
             else:
                self.count = 15
-            return TargetAllocation({ticker: 0 for ticker in self.tickers})
+            allocation_dict = {ticker: 0 for ticker in self.tickers}
+            return TargetAllocation(allocation_dict)
          elif self.count < 1:
             #allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
-            if self.equal_weighting: 
-               allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
-            else:
-               allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
+            allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
             return TargetAllocation(allocation_dict)
          else:
-            return TargetAllocation({ticker: 0 for ticker in self.tickers})
+            allocation_dict = {ticker: 0 for ticker in self.tickers}
+            return TargetAllocation(allocation_dict)
 
       else:
          return TargetAllocation(allocation_dict)
