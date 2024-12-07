a4c9671acf1c799cdd666c2a48613cc413dd98eb
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-12-07 22:32:46 +0500

    User Simone Pantaleoni saved changes to main.py at 12/07/2024, 17:32:45 UTC time.

--- a837192f-2906-4dc2-86ad-86e925768892/main.py
+++ a837192f-2906-4dc2-86ad-86e925768892/main.py
@@ -73,13 +73,13 @@ class TradingStrategy(Strategy):
                allocation_dict = {ticker: 0 for ticker in self.tickers}
 
             elif self.count < 1 and mrktClose > mrktEMA[-2]:
-               allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
-               for i in self.data_list:
-                  if tuple(i)[0]=="insider_trading":
-                     if data[tuple(i)] and len(data[tuple(i)])>0:
-                        sales = len([i for i in data[tuple(i)][-20:] if "Sale" in i['transactionType']])
-                        if sales/20 > 0.4:
-                           allocation_dict[tuple(i)[1]] = 0
+               allocation_dict = {i: 1/len(self.tickers` for i in self.tickers}
+               #for i in self.data_list:
+                  #if tuple(i)[0]=="insider_trading":
+                     #if data[tuple(i)] and len(data[tuple(i)])>0:
+                        #sales = len([i for i in data[tuple(i)][-20:] if "Sale" in i['transactionType']])
+                        #if sales/20 > 0.4:
+                           #allocation_dict[tuple(i)[1]] = 0
             else:
                allocation_dict = {ticker: 0 for ticker in self.tickers}
 
