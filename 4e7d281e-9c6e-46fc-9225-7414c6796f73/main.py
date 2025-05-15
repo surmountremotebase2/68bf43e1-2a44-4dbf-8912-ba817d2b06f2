c6e459ead0c48deb28cf809d0796e7b14da739e4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-05-15 23:20:16 +0500

    User Simone Pantaleoni saved changes to main.py at 05/15/2025, 18:20:15 UTC time.

--- 4e7d281e-9c6e-46fc-9225-7414c6796f73/main.py
+++ 4e7d281e-9c6e-46fc-9225-7414c6796f73/main.py
@@ -17,7 +17,7 @@ class TradingStrategy(Strategy):
             MedianCPI(),
             StickyPriceConsumerPriceIndex()
         ]
-        self.tickers = []  # Will be populated dynamically
+        self.tickers = ["SPY"]  # Will be populated dynamically
 
     @property
     def interval(self):
