817bfc39bc9044cb202348fcae31866246ecc7ec
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-06-23 23:19:35 +0500

    User Simone Pantaleoni saved changes to main.py at 06/23/2025, 18:19:34 UTC time.

--- 3916f07f-8f5d-402c-922f-2a02bb20d386/main.py
+++ 3916f07f-8f5d-402c-922f-2a02bb20d386/main.py
@@ -1,5 +1,6 @@
 from surmount.base_class import Strategy, TargetAllocation
 from surmount.data import MedianCPI
+from surmount.technical_indicators import EMA, VWAP
 from surmount.logging import log
 import pandas as pd
 
@@ -120,8 +121,9 @@ class TradingStrategy(Strategy):
 
         # Calculate quarterly VWAP using the helper method.
         vwap_series = self._vwap(market_df['high'], market_df['low'], market_df['close'], market_df['volume'], anchor_period='quarter')
+        current_vwap = VWAP(self.market_benchmark, data["ohlcv"], 82)[-1]
         
-        current_vwap = vwap_series.iloc[-1]
+        #current_vwap = vwap_series.iloc[-1]
         current_close = market_df['close'].iloc[-1]
         
         # If market benchmark close is below its quarterly VWAP, trigger risk-off state.
