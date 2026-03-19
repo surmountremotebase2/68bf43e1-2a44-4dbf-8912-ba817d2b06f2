2c992ebd60483afbd85cb857cf83ce448e22d933
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-03-19 21:03:46 +0100

    User Simone Pantaleoni saved changes to main.py at 03/19/2026, 20:03:45 UTC time.

--- d335a535-d48e-456a-befb-9b4f88e18a49/main.py
+++ d335a535-d48e-456a-befb-9b4f88e18a49/main.py
@@ -18,7 +18,7 @@ class TradingStrategy(Strategy):
       atr = ATR("TQQQ", d, 40)
       qqq_stake = 0
       
-      if len(d)>3 and (("13:00" or "13:30") in d[-1]["TQQQ"]["date"]) and atr[-1]/d[-1]["TQQQ"]["close"]>0.001:
+      if len(d)>3 and "13:30" in d[-1]["TQQQ"]["date"] and atr[-1]/d[-1]["TQQQ"]["close"]>0.001:
          v_shape = d[-2]["TQQQ"]["close"]<d[-3]["TQQQ"]["close"] and d[-1]["TQQQ"]["close"]>d[-2]["TQQQ"]["close"]
          log(str(v_shape))
          log(d[-1]["TQQQ"]["date"])
