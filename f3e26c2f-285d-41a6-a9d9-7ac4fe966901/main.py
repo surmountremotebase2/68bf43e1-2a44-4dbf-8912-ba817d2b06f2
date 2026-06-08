7d776ca3ba1620e10516c42162a45405f1e0e6c5
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-06-08 11:42:05 -0700

    User Simone Pantaleoni saved changes to main.py at 06/08/2026, 18:42:04 UTC time.

--- f3e26c2f-285d-41a6-a9d9-7ac4fe966901/main.py
+++ f3e26c2f-285d-41a6-a9d9-7ac4fe966901/main.py
@@ -80,7 +80,7 @@ class TradingStrategy(Strategy):
             allocation = 1.0
         else:
             prev_bar = current_day_bars[-2]
-            if prev_bar["close"] > opening_high and current_close < latest_vwap:
+            if prev_bar["close"] > opening_high or current_close > latest_vwap:
                 allocation = 1.0
             else:
                 allocation = 0.0
