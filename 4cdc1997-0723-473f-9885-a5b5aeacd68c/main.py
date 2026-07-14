1fd2200b3b8230ba61dfd238e36e14c98017a223
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-07-14 23:36:28 +0500

    User Simone Pantaleoni saved changes to main.py at 07/14/2026, 18:36:27 UTC time.

--- 4cdc1997-0723-473f-9885-a5b5aeacd68c/main.py
+++ 4cdc1997-0723-473f-9885-a5b5aeacd68c/main.py
@@ -41,7 +41,7 @@ class TradingStrategy(Strategy):
 
         # 2. Calculate the 20-month (420-day) EMA for the ratio
         spy_gld_ema50 = ta.sma(spy_gld_ratio, length=lookback).tolist()
-        spy_gld_ema3 = ta.ema(spy_gld_ratio, length=3).tolist()
+        spy_gld_ema3 = ta.ema(spy_gld_ratio, length=5).tolist()
         #current_ratio = spy_gld_ratio.tolist()[-1]
         current_ratio = spy_gld_ema3[-1]
         current_ema = spy_gld_ema50[-1]
