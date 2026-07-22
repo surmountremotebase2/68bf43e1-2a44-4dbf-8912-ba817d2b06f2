12fd45b3c37a5f8a9705414d01e7b0eec12f6af4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-07-22 23:04:36 +0500

    User Simone Pantaleoni saved changes to main.py at 07/22/2026, 18:04:35 UTC time.

--- 4cdc1997-0723-473f-9885-a5b5aeacd68c/main.py
+++ 4cdc1997-0723-473f-9885-a5b5aeacd68c/main.py
@@ -6,7 +6,7 @@ import pandas_ta as ta
 class TradingStrategy(Strategy):
     def __init__(self):
         # We only need SPY and GLD for this rotation strategy
-        self.tickers = ["SPY", "GLD", "QQQ"]
+        self.tickers = ["GLD", "QQQ"]
         self.data_list = []
 
     @property
@@ -33,28 +33,28 @@ class TradingStrategy(Strategy):
             return TargetAllocation({})
 
         # Extract closing prices
-        spy_close = pd.Series([x["QQQ"]["close"] for x in ohlcv])
+        qqq_close = pd.Series([x["QQQ"]["close"] for x in ohlcv])
         gld_close = pd.Series([x["GLD"]["close"] for x in ohlcv])
 
         # 1. Calculate the SPY/GLD ratio
-        spy_gld_ratio = spy_close / gld_close
+        qqq_gld_ratio = qqq_close / gld_close
 
         # 2. Calculate the 20-month (420-day) EMA for the ratio
-        spy_gld_ema50 = ta.sma(spy_gld_ratio, length=lookback).tolist()
-        spy_gld_ema3 = ta.ema(spy_gld_ratio, length=5).tolist()
+        qqq_gld_ema50 = ta.sma(qqq_gld_ratio, length=lookback).tolist()
+        qqq_gld_ema3 = ta.ema(qqq_gld_ratio, length=5).tolist()
         #current_ratio = spy_gld_ratio.tolist()[-1]
-        current_ratio = spy_gld_ema3[-1]
-        current_ema = spy_gld_ema50[-1]
+        current_ratio = qqq_gld_ema3[-1]
+        current_ema = qqq_gld_ema50[-1]
 
         # Initialize base weights
-        spy_weight = 0.0
+        qqq_weight = 0.0
         gld_weight = 0.0
 
         # 3. Apply the rotation logic
         # If the ratio is above its 20-month EMA, we are in a Risk-On regime (Buy SPY)
         # Otherwise, Risk-Off regime (Buy GLD)
         if current_ratio > current_ema:
-            spy_weight = 1.0
+            qqq_weight = 1.0
         else:
             gld_weight = 1.0
 
@@ -62,6 +62,6 @@ class TradingStrategy(Strategy):
         #log(f"SPY/GLD Ratio: {current_ratio:.4f} | 20M EMA: {current_ema:.4f} -> Allocating SPY: {spy_weight}, GLD: {gld_weight}")
 
         return TargetAllocation({
-            "QQQ": spy_weight,
+            "QQQ": qqq_weight,
             "GLD": gld_weight
         })
